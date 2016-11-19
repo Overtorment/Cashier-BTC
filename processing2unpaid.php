be38f6d44014c91eec568e483c45b0386f9694c8
Overtorment <overtorment@gmail.com>
2016-11-19 05:37:13 -0500

    ssl fix

--- processing2unpaid.php
+++ processing2unpaid.php
@@ -5,7 +5,7 @@ set_time_limit(0);
 ini_set('memory_limit', ' -1');
 
 
-$config_lines = file('config.js');
+$config_lines = file(__DIR__.'/config.js');
 
 foreach ($config_lines as $key => $line) {
 	if (stripos($line, '"couchdb"') !== false){
@@ -40,8 +40,15 @@ $json = file_get_contents("{$couchdb}/_design/address/_view/processing_by_timest
 $json = json_decode($json, true);
 
 foreach($json['rows'] as $row){
-    $balance = file_get_contents("{$base_url}/get_address_confirmed_balance/{$row['id']}");
-    $document = file_get_contents("{$couchdb}/{$row['id']}");
+	$arrContextOptions=array(
+	    "ssl"=>array(
+	        "verify_peer"=>false,
+	        "verify_peer_name"=>false,
+	    ),
+	);  
+
+    $balance = file_get_contents("{$base_url}/get_address_confirmed_balance/{$row['id']}", false, stream_context_create($arrContextOptions));
+    $document = file_get_contents("{$couchdb}/{$row['id']}", false, stream_context_create($arrContextOptions));
     $document = json_decode($document, true);
     unset($document['processing']);
     $document['processed'] = "unpaid";
