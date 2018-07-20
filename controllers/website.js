26f8729b4dd5d7cee710e7f29a93762322fd6018
Micx Samonte <micx.samonte@gmail.com>
2018-07-20 22:47:31 +0800

    /generate_qr filename bugfix

--- controllers/website.js
+++ controllers/website.js
@@ -21,7 +21,7 @@ router.get('/generate_qr/:text', function (req, res) {
   qrSvg = qr.image(decodeURIComponent(req.params.text), { type: 'png' })
   qrSvg.pipe(fs.createWriteStream(filename))
   qrSvg.on('end', function () {
-    res.redirect(301, `${redirectPath}/filename`)
+    res.redirect(301, `${redirectPath}/${filename}`)
     res.end()
   })
   qrSvg.on('error', function () {
